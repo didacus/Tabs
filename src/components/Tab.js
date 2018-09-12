34afb5effb5d573017c5ec3fa4fd55ce7369a34a
diegooriani <diegooriani@hotmail.com>
2018-09-12 16:44:37 +0100

    Ajusted tab size.

--- src/components/Tab.js
+++ src/components/Tab.js
@@ -3,12 +3,12 @@ import styled from 'styled-components'
 
 const Tab = ({ onClick, isSelected, children }) => {
 	const TabWrapper = styled.li`
-		width: 48px;
 		display: flex;
 		align-items: center;
 		justify-content: center;
-		padding: 20px;
+		padding: 100px;
 		margin: 1px;
+		font-size: 3em;
 		color: ${props => (isSelected ? `white` : `black`)};
 		background-color: ${props => (isSelected ? `black` : `#C4C4C4`)};
 		cursor: ${props => (isSelected ? 'default' : `pointer`)};
